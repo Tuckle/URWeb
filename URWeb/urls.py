aaff4ce24c74adb3a265adb448823f2b33ce0e2d
Georgiana Ingrid STOLERU <istoleru@ias.bitdefender.biz>
2017-04-20 19:17:06 +0300

    Changes in design and Login functionality

--- URWeb/urls.py
+++ URWeb/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from .views import home
+from URWeb import home
+from URWeb.login import LoginView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', home.Home.as_view(), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^admin',  admin.site.urls),
+    url(r'^login',  LoginView.as_view(template_name="login.html"), name='login'),
+    url(r'^signup', TemplateView.as_view(template_name='signup.html'), name='signup'),		
+    url(r'^myaccount', TemplateView.as_view(template_name='myaccount.html'), name='myaccount')
 ]
--- URWeb/views/home.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.views import generic, View
-
-
-class Home(generic.TemplateView):
-    template_name = "home.html"
