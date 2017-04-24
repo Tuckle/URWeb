7fdddddce6b0f0326b0601f84da2c7df7d9adfaf
Georgiana Ingrid STOLERU <istoleru@ias.bitdefender.biz>
2017-04-24 03:17:14 +0300

    signup ok && reset pass - need smtp finish

--- URWeb/app/authentication/urls.py
+++ URWeb/app/authentication/urls.py
@@ -14,19 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from .views.signup import SignupView
+#from .views.signup import SignupView
 from .views.myaccount import MyAccountView
-from .views.forgotpassword import ForgotPasswordView
+#from .views.forgotpassword import ForgotPasswordView
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from .views import views as core_views
 
 auth_urls = [
     #url(r'^login',  LoginView.as_view(template_name="login.html"), name='login'),
     url(r'^login',  auth_views.login, {'template_name': 'login.html', "redirect_authenticated_user": True}, name='login'),
     url(r'^logout', auth_views.logout, {'template_name': 'logout.html', 'next_page': '/'}, name='logout'),
     #url(r'^logout', LogoutView.as_view(template_name="logout.html"), name='logout')
-    url(r'^signup', SignupView.as_view(template_name="signup.html"), name='signup'),
+    #url(r'^signup', SignupView.as_view(template_name="signup.html"), name='signup'),
     url(r'^myaccount', MyAccountView.as_view(template_name='myaccount.html'), name='myaccount'),
-    url(r'^forgotpassword', ForgotPasswordView.as_view(template_name="forgotpassword.html"), name='forgotpassword'),
+    url(r'^password_reset', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})',auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^signup', core_views.signup, name='signup'),
+    #url(r'^forgotpassword', ForgotPasswordView.as_view(template_name="forgotpassword.html"), name='forgotpassword'),
 ]
