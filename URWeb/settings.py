8808bf7fcdb5fa6104d6d4ecc1234fad20640efe
Tuckle <adi.piriu5113@yahoo.com>
2017-04-26 02:16:35 +0300

    Fixed design bug

--- URWeb/settings.py
+++ URWeb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = r'&ncu9ucx_4oz9_k$k&%=b=3^2=!&vn-8i01qm5#pn1=_996_#u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [u'URWeb.pythonanywhere.com', u'127.0.0.1', "home.com"]
 
 
 # Application definition
