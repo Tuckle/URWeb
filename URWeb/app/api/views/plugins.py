e1bfbd369a81e5743010756051a976f0051798db
Gheorghe BALAN <gbalan@ias.bitdefender.biz>
2017-05-31 01:06:16 +0300

    several updates

--- URWeb/app/api/views/plugins.py
+++ URWeb/app/api/views/plugins.py
@@ -4,8 +4,10 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 import json
+import re
 import os
 import pickle
+import zipfile
                                 
 class Plugins(generic.View):
 
@@ -23,6 +25,16 @@ class Plugins(generic.View):
 		pFile = open(self.pluginsPathList, "rb")
 		self.dictObject = pickle.load(pFile)
 		pFile.close()
+
+	def refresh_plugin_list(plugin_name, plugin_description, plugin_path):
+		self.loadDict()
+		self.dictObject.append({
+		    'name': plugin_name,
+		    'description': plugin_description,
+		    'path': plugin_path
+		})
+		self.saveDict()
+		
 		
 	def get(self, request, name, format):
 		if not name:
@@ -54,11 +66,34 @@ class Plugins(generic.View):
 			return HttpResponse(json.dumps(data))
 
 	def put(self, request, name, format):
+		print(str(request.body))
 		data = json.loads(request.body)
-		if data['name'] == 'adi_plugin':
-			return HttpResponse("OK")
-		else:
+
+		print(str(data))
+		
+		file_path = os.path.join(self.pluginModulesPath, file_name)
+		if not os.path.exists(file_path):
+			os.makedirs(file_path)
+		arc_handle = zipfile.ZipFile(arc_path, "r")
+		arc_handle.extractall(file_path)
+		arc_handle.close()
+
+		# file checks
+		pname_validation = re.match("^[\w]+.((zip)|(rar))$", data['name'])
+		if not pname_validation:
 			return HttpResponse("Bad plugin name")
+		#if data['name'] == 'adi_plugin':
+		#	return HttpResponse("OK")
+		#else:
+		#	return HttpResponse("Bad plugin name")
+		# retrieve data about plugin to be uploaded(name, desc, path)
+		file_name = None
+		file_description = None
+		arc_path = None #  archive path
+
+		self.refresh_plugin_list(file_name, file_description, arc_path, file_path)
+
+		return HttpResponse("OK")
 
 	def post(self, request, name, format):
 		if not name:
