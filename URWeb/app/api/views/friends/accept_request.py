ae895b7d28cab2a1e269302b4f04ef0623e63d4c
Gheorghe BALAN <gbalan@ias.bitdefender.biz>
2017-06-01 14:28:07 +0300

    gigi + iulian commits

---
+++ URWeb/app/api/views/friends/accept_request.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+import json
+import re
+import os
+import pickle
+import zipfile
+
+class AcceptRequest(generic.View):
+	def get(self, request, username):
+		if not username:
+			response = dict()
+			return HttpResponse(json.dumps(response))
+		else:
+			data = dict()
+			return HttpResponse(json.dumps(data))
+
+	def put(self, request, username):
+		if not username:
+			response = dict()
+			return HttpResponse(json.dumps(response))
+		else:
+			data = dict()
+			return HttpResponse(json.dumps(data))		
+		
---
+++ URWeb/app/api/views/friends/friend_requests.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+import json
+import re
+import os
+import pickle
+import zipfile
+
+class FriendRequests(generic.View):
+	def get(self, request, username):
+		if not username:
+			response = dict()
+			return HttpResponse(json.dumps(response))
+		else:
+			data = dict()
+			return HttpResponse(json.dumps(data))
